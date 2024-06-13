996ef0c609e6cc59479d2901ff241d6c9e3785cf
Leonardo Martins <lmaleomartins@gmail.com>
2024-06-12 21:43:42 -0300

    authentication?

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:habit_tracker/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 import 'pages/home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(ChangeNotifierProvider(
